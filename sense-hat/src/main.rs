809aa0c96705a8205c90ff70425a9f7f2b12825a
flo <inbox.733@gmail.com>
2022-06-28 14:13:56 -0600

    Added sensehat temp reading from humidity sensor.

--- sense-hat/src/main.rs
+++ sense-hat/src/main.rs
@@ -1,6 +1,7 @@
 mod led;
 
 use led::screen::LedControls;
+use sensehat::SenseHat;
 
 fn main() {
     let mut led = LedControls::new();
@@ -8,5 +9,9 @@ fn main() {
     led.scroll_text("hello world");
     led.display_symbol(&led::HALLOWEEN, 12);
 
-    println!("done!");
+    let mut hat = SenseHat::new().unwrap();
+    let temp = hat.get_temperature_from_humidity().unwrap().as_celsius();
+
+
+    println!("Temp: {:?} C", temp);
 }
